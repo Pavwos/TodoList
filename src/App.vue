c0ba21f45ad9c56e96ae722d59ef061fd482b88a
pavwos <rspavanelli@gmail.com>
2024-10-30 21:35:38 -0300

    checks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <div id="app" class="h-screen flex flex-col items-center justify-center">
-    <h1 class="text-5xl m-4 font-bold">TodoList</h1>
-    <TodoList/>
+  <div id="app">
+    <main  class="flex-grow flex flex-col items-center gap-4">
+      <h1 class="text-5xl m-4">TodoList</h1>
+      <TodoList/>
+    </main>
+    <footer class="flex flex-col items-center">
+      <p>@pavwos © 2024</p>
+    </footer>
   </div>
 </template>
 
@@ -13,3 +18,27 @@
     }
   };
 </script>
+
+<style scoped>
+  #app{
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    background-color: #F9FAFB;
+    font-family: sans-serif;
+  }
+  h1{
+    color: #22c55e;
+    text-shadow: 0px 0px 5px #000;
+    font-family: sans-serif;
+    font-weight: bolder;
+  }
+  main{
+    flex-grow: 1;
+  }
+  footer{
+    margin-top: 15px;
+    padding: 10px;
+    background-color: #E5E7EB;
+  }
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="TL">
-    <main class="flex flex-col items-center gap-4">
-      <input v-model="newTask" placeholder="Type your task..." @keyup.enter="addTask" class="border-2 border-black rounded-lg p-1" >
-      <button @click="addTask" class="bg-green-600 text-slate-100 p-2 rounded-md w-20 font-bold">Add</button>
-      <ul class="flex flex-col gap-2">
-        <li v-for="(task, index) in tasks" :key="index" class="w-72 bg-slate-500 p-2 text-neutral-50 flex flex-row items-center justify-between gap-4 rounded-lg">
-          {{ task }}
-          <button @click="rmTask(index)" class="bg-red-500 text-slate-100 p-2 rounded-md w-20">Delete</button>
-        </li>      
+    <main class="flex flex-col items-center justify-between flex-wrap gap-4  w-full">
+      <div id="inputField" class="flex flex-row gap-2">
+        <input v-model="newTask" placeholder="Type your task..." @keyup.enter="addTask" class="border-2 border-black rounded-lg p-1" >
+        <button id="add" @click="addTask" class="bg-green-600 text-slate-100 p-2 rounded-md w-20 font-bold">Add</button>
+      </div>
+      <div id="count">
+        <p>Tasks: {{ tasks.length }}</p>
+        <p>Checked: {{ completedTasks }}</p>
+        <p>To-Do: {{ incompletedTasks }}</p>
+      </div>
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index" :class="{'line-through text-gray-500': task.completed}">
+          {{ task.text }}
+          <div id="btns">
+            <button id="check" @click="check(index)" class="bg-green-500 text-slate-100 p-2 rounded-lg w-20 m-2">Check</button>
+            <button id="del" @click="rmTask(index)" class="bg-red-500 text-slate-100 p-2 rounded-lg w-20">Delete</button>
+          </div>
+        </li>
       </ul>
     </main>
-    <footer class="flex flex-col items-center font-light">
-      <p>@pavwos</p>
-    </footer>
   </div>
 </template>
 <script>
@@ -26,13 +33,78 @@ export default {
 	methods: {
 		addTask() {
 			if (this.newTask.trim() !== '') {
-				this.tasks.push(this.newTask);
+				this.tasks.push({ text: this.newTask, completed: false});
 				this.newTask = "";
 			}
 		},
 		rmTask(index) {
 			this.tasks.splice(index, 1);
-		}
-	}
+		},
+    check(index){
+      this.tasks[index].completed = !this.tasks[index].completed
+    }
+	},
+  computed: {
+    completedTasks(){
+      return this.tasks.filter(task => task.completed).length;
+    },
+    incompletedTasks(){
+      return this.tasks.filter(task => !task.completed).length;
+    }
+  }
 };
 </script>
+<style scoped>
+  #count{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
+  p{
+    color: #374151;
+  }
+  button{
+    transition: 0.5s;
+    border: 1px solid #22c55e;
+  }
+  input{
+    background-color: #cbd5e1;
+    color: #000;
+    outline: none;
+    border: 1px solid #22c55e;
+    transition: all 0.5s;
+    padding: 10px 15px;
+  }
+  input:hover{
+    border: 1px solid #22c55e;
+    box-shadow: 1px 1px 10px #22c55e;
+  }
+  input::placeholder{
+    color: #000
+  }
+  #add:hover{
+    border: 1px solid #22c55e;
+    box-shadow: 1px 1px 10px #22c55e;
+  }
+  ul{
+    min-width: 50vh;
+    max-width: 100vh;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+  li{
+    font-size: 18px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    background-color: #d1ffe2;
+    padding: 5px 10px;
+    border: 2px solid #22c55e;
+    border-radius: 5px;
+    margin: 5px;
+  }
+</style>
